c84a45386c964b862c09aac3ea110babf894468d
Drew Botka <hellodrewbot@gmail.com>
2014-11-07 10:44:21 -0500

    created a global variable for backbone components and adjusted input type on hover

---
+++ app/scripts/var-global.js
@@ -0,0 +1,51 @@
+"use strict";
+
+// global variable to house/access all Constructors
+var Demi = {
+	Models: {
+		Course: {},
+		Timeline: {},
+		Week: {},
+		Goals: {},
+		Resources: {},
+		Assignments: {},
+	},
+	Collections: {
+		CourseCollection: {},
+		TimelineCollection: {},
+		WeekCollection: {},
+		GoalsCollection: {},
+		ResourcesCollection: {},
+		AssignmentsCollection: {},
+	},
+	Views: {
+		WeekView: {},
+		AppView: {},
+	},
+	Routers: {
+
+	},
+	models: {
+		course: {},
+		timeline: {},
+		week: {},
+		goals: {},
+		resources: {},
+		assignments: {},
+	},
+	collections: {
+		courseCollection: {},
+		timelineCollection: {},
+		weekCollection: {},
+		goalsCollection: {},
+		resourcesCollection: {},
+		assignmentsCollection: {},
+	},
+	views: {
+		weekView: {},
+		appView: {},
+	},
+	routers: {
+
+	},
+};
